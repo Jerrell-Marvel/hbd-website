0db7adbf36d08e72b821ae92c38206be1672fb38
Jerrell Marvel <jerrell.marvel88@gmail.com>
2023-01-25 00:59:50 +0700

    change meta description

--- pages/index.tsx
+++ pages/index.tsx
@@ -30,7 +30,7 @@ export default function Home() {
     <>
       <Head>
         <title>Happy Birthday!</title>
-        <meta name="description" content="Jerrell Marvel's personal portfolio website" />
+        <meta name="description" content="Created by Nathanael Christian" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
 
